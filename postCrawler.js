2449089bebfeac20ecaee3f4d7ad7289e97c1434
p-herbert <pete.a.herbert@gmail.com>
2016-09-23 16:49:46 -0700

    (feat) Create class PostCrawler

---
+++ postCrawler.js
@@ -0,0 +1,31 @@
+const cheerio = require('./node_modules/cheerio');
+const request = require('./node_modules/request');
+
+class PostCrawler {
+
+  constructor(url) {
+    this.url = url;
+    this.$ = null;
+
+    this.postInfo = {
+      author: null,
+      title: null,
+      url: this.url,
+      tags: [],
+      links: [] };
+  }
+
+  get(cb) {
+    request(this.url, (err, response, body) => {
+      if (err) {
+        cb(err, null);
+        return;
+      }
+
+      this.$ = cheerio.load(body);
+      cb(null, this.$);
+    });
+  }
+
+}
+
