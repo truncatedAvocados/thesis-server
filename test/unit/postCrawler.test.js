865b650c23f4fd7eabe224a504a62b407f532496
p-herbert <pete.a.herbert@gmail.com>
2016-09-24 14:51:21 -0700

    (test) Fetch html content

--- test/unit/postCrawler.test.js
+++ test/unit/postCrawler.test.js
@@ -1,5 +1,18 @@
 const assert = require('assert');
+const PostCrawler = require('../../postCrawler');
 
 describe('PostCrawler Tests', () => {
+  it('Create PostCrawler instance', () => {
+    const url = 'http://blog.edankwan.com/post/three-js-advanced-tips-shadow';
 
+    const crawler = new PostCrawler(url);
+
+    crawler.get((err, $) => {
+      if (crawler.html() === null) {
+        assert.ok(false);
+      } else {
+        assert.ok(true);
+      }
+    });
+  });
 });
