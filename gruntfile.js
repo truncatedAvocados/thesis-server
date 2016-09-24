952b1dde13a33cf15228462621a7f34a918314a6
p-herbert <pete.a.herbert@gmail.com>
2016-09-23 23:21:01 -0700

    (fix, refactor) Fix typo and refactor module

--- gruntfile.js
+++ gruntfile.js
@@ -1,18 +1,18 @@
 module.exports = (grunt) => {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
-    mocha: {
+    mochaTest: {
       test: {
-        sre: ['test/unit/*.sj'] } },
+        src: ['test/unit/*.js'] } },
     karma: {
       unit: {
         configFile: 'karma.conf.js' } } });
 
   grunt.loadNpmTasks('grunt-karma');
-  grunt.loadNpmTasks('grunt-mocha');
+  grunt.loadNpmTasks('grunt-mocha-test');
 
   grunt.registerTask('unit', () => {
-
+    grunt.task.run(['mochaTest']);
   });
 
   grunt.registerTask('integration', () => {
