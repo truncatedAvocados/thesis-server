95edb8c129d8ad2a76e1e186f5621f3d4b5cabf6
abandeali1 <bandeali1@gmail.com>
2016-09-24 16:02:45 -0700

    (feat) Add regex for filtering urls

--- frontPageCrawler.js
+++ frontPageCrawler.js
@@ -1,36 +1,77 @@
 var cheerio = require('cheerio');
 var request = require('request');
 var fs = require('fs');
+var whitelist = require('./whitelist.json');
 
 
-var getNewBlogPosts = (urlList, callback) => {
-	var queue = [];
+module.exports = {
+	getNewBlogPosts: (urlList, callback) => {
+		var result = [];
+		var added = {};
+		var filters = ['header', 'footer', 'aside', 'nav', '.nav', '.navbar'];
+		var startTime = new Date().getTime();
+		var resultCount = 0;
+		var regex = /http\w*\:\/\/(www\.)?/i;
 
-	var addPosts = (count) => {
-		request(urlList[count], (err, res, html) => {
-			if (err) {
-				throw err;
-			} else {
-				var $ = cheerio.load(html);
-				var anchors = $('a');
-				for (var key in anchors) {
-					if (anchors[key].attribs) {
-						if (anchors[key].attribs.href) {
-							queue.push(anchors[key].attribs.href);
+		var addPosts = (count) => {
+			var frontPageUrl = urlList[count];
+			request(frontPageUrl, (err, res, html) => {
+				if (err) {
+					console.log(err);
+				} else {
+					var regUrl = frontPageUrl.slice(regex.exec(frontPageUrl)[0].length);
+					var $ = cheerio.load(html);
+					console.log('Before: ', Object.keys($('a')).length);
+					filters.forEach((filter) => {
+						$(filter).empty();
+					});
+					console.log('After: ', Object.keys($('a')).length);
+					var anchors = $('a');
+					for (var key in anchors) {
+						if (anchors[key].attribs) {
+							var blogPostUrl = anchors[key].attribs.href;
+							if (blogPostUrl && !added[blogPostUrl]) {
+								var regBlogUrl = regex.exec(blogPostUrl) ? blogPostUrl.slice(regex.exec(blogPostUrl)[0].length) : blogPostUrl;
+								console.log('BLOG: ', regBlogUrl);
+								console.log('FRONT: ', regUrl);
+								//console.log(regBlogUrl.slice(0, regUrl.length) + ' ' + regUrl);
+								if (regBlogUrl.slice(0, regUrl.length) === regUrl) {
+									added[blogPostUrl] = true;
+									result.push(blogPostUrl);
+								}
+							}
 						}
 					}
 				}
+				// console.log('New Links: ', result.length - resultCount);
+				// console.log('Total Links: ', result.length);
+				// console.log('Time: ', new Date().getTime() - startTime);
+				// console.log('URL: ', frontPageUrl);
+				resultCount = result.length;
 				if (urlList[count + 1]) {
 					addPosts(count + 1);
 				} else {
-					callback(queue.length);
+					callback(result);
 				}
-			}
-		});
-	};
-
-	addPosts(0);
+			});
+		};
+		addPosts(0);
+	}
 };
 
-getNewBlogPosts(['https://tech.instacart.com/'], console.log);
+var whiteListKeys = Object.keys(whitelist);
+module.exports.getNewBlogPosts(whiteListKeys, (result) => {
+	console.log(result.length);
+	var toWrite = {};
+	for (var i = 0; i < 500; i++) {
+		toWrite[result[i]] = true;
+	}
+	fs.writeFile('output.json', JSON.stringify(toWrite), (err) => {
+		if (err) {
+			console.log(err);
+		}
+	});
+});
 //'https://infrequently.org/'
+//['http://android-developers.blogspot.com/']
+//other elements/classes to filter out: li? footer
