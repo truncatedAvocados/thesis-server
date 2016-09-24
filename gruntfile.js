f8b355382192879668b9fc8abf0c829b5da1ea1e
p-herbert <pete.a.herbert@gmail.com>
2016-09-24 00:15:52 -0700

    (refactor) Disable karma task

--- gruntfile.js
+++ gruntfile.js
@@ -20,7 +20,7 @@ module.exports = (grunt) => {
   });
 
   grunt.registerTask('integration', () => {
-    grunt.task.run(['karma']);
+//  grunt.task.run(['karma']);
   });
 
   grunt.registerTask('converage', () => {
