0fd90ee33d962f922e64027049b58e286f4b7722
p-herbert <pete.a.herbert@gmail.com>
2016-09-24 16:07:01 -0700

    (test, refactor) Implement async unit tests

--- test/unit/postCrawler.test.js
+++ test/unit/postCrawler.test.js
@@ -2,17 +2,28 @@ const assert = require('assert');
 const PostCrawler = require('../../postCrawler');
 
 describe('PostCrawler Tests', () => {
-  it('Create PostCrawler instance', () => {
-    const url = 'http://blog.edankwan.com/post/three-js-advanced-tips-shadow';
+  const url = 'http://blog.edankwan.com/post/three-js-advanced-tips-shadow';
+  const crawler = new PostCrawler(url);
 
-    const crawler = new PostCrawler(url);
-
+  beforeEach((done) => {
     crawler.get((err, $) => {
-      if (crawler.html() === null) {
-        assert.ok(false);
-      } else {
-        assert.ok(true);
-      }
+      done();
     });
+  });
+
+  it('Create PostCrawler instance', () => {
+    if (crawler.html() === null) {
+      assert.ok(false);
+    } else {
+      assert.ok(true);
+    }
+  });
+
+  it('Get post title', () => {
+    if (crawler.getTitle().length > 0) {
+      assert.ok(false);
+    } else {
+      assert.ok(true);
+    }
   });
 });
