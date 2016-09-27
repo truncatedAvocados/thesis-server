0ae369c4029dd6c46b388d9eb2360c12d7aceb8d
p-herbert <pete.a.herbert@gmail.com>
2016-09-26 20:05:55 -0700

    (feat) Add tags get and set methods

--- postCrawler.js
+++ postCrawler.js
@@ -1,5 +1,7 @@
 const cheerio = require('./node_modules/cheerio');
 const request = require('./node_modules/request');
+const natural = require('./node_modules/natural');
+const stopwords = require('./node_modules/stopwords').english;
 
 class PostCrawler {
 
@@ -25,6 +27,7 @@ class PostCrawler {
       this.$ = cheerio.load(body);
       this.setTitle();
       this.setLinks();
+      this.setTags();
 
       cb(null, this.$);
     });
@@ -54,12 +57,54 @@ class PostCrawler {
     const redirectRegEx = /^\//;
     let href;
 
+    // Remove old links
+    this.postInfo.links = [];
+
     this.$('#content, #main, .post, .entry').find('a').each((i, elem) => {
       href = this.$(elem).attr('href');
       if (!redirectRegEx.test(href)) {
         this.postInfo.links.push(href);
       }
     });
+  }
+
+  getTags() {
+    return this.postInfo.tags;
+  }
+
+  setTags() {
+    const TfIdf = natural.TfIdf;
+    const tfidf = new TfIdf();
+    const anchors = this.$('a[rel=tag]');
+    let tag;
+    let p;
+
+    // Remove old tags
+    this.postInfo.tags = [];
+
+    // Look for tag anchors
+    if (anchors.length > 0) {
+      anchors.each((i, elem) => {
+        tag = this.$(elem).text();
+        if (this.postInfo.tags.indexOf(tag) === -1) {
+          this.postInfo.tags.push(tag);
+        }
+      });
+    // Natural Language Process to assign tags
+    } else {
+      this.$('#content, #main, .post, .entry').find('p').each((i, elem) => {
+        p = this.$(elem).text();
+        // tf-idf score
+        tfidf.addDocument(p);
+      });
+
+      tfidf.listTerms(0).slice(0, 5).forEach((item) => {
+        // Exclude stopwords
+        if (stopwords.indexOf(item.term.toLowerCase()) === -1) {
+          this.postInfo.tags.push(item.term);
+        }
+      });
+    }
   }
 }
 
