e3a9f0437a041dd27eb1465c9149d01135cc5df1
abandeali1 <bandeali1@gmail.com>
2016-09-27 19:03:03 -0700

    (refactor) Export scheduler functions

--- scheduler.js
+++ scheduler.js
@@ -1,6 +1,3 @@
-var frontPageCrawler = require('./frontPageCrawler.js');
-var whitelist = require('./whitelist.json');
-var fs = require('fs');
 var numCPUs = require('os').cpus().length;
 var cluster = require('cluster');
 var baseUrls = require('./baseUrls.json');
@@ -10,69 +7,46 @@ var getBaseUrl = (url) => {
 	return regex.exec(url)[2];
 };
 
-var scheduleCrawlers = (queue) => {
-	if (cluster.isMaster) {
-		var workers = numCPUs;
-		var messageHandler = (message) => {
-			if (message.type === 'finish') {
-				workers--;
+module.exports = {
+	scheduleCrawlers: (queue) => {
+		if (cluster.isMaster) {
+			var workers = numCPUs;
+			for (var i = 0; i < workers; i++) {
+				cluster.fork();
 			}
-		};
-		for (var i = 0; i < workers; i++) {
-			var worker = cluster.fork();
-		}
-	} else {
-		var startLength = queue.length;
-		var traverseArray = (count) => {
-			if (baseUrls[getBaseUrl(queue[count])]) {
-				console.log(queue[count]);
-			}
-			var next = count + numCPUs >= startLength ? count + 1 : count + numCPUs;
-			if (queue[next]) {
-				traverseArray(next);
-			} else {
-				cluster.worker.kill();
-			}
-		};
-		var id = cluster.worker.id - 1;
-		traverseArray(id);
-	}
-};
-
-// var scheduleCrawlersSingle = (queue) {
-// 	while (queue.length > 0) {
-// 		var url = queue.shift();
-// 		//do some regex
-// 		if (baseUrls[getBaseUrl(url)]) {
-// 			crawl(queue.shift(), (result) => {
-// 				queue.concat(result);
-// 			});
-// 		}
-// 	}
-// };
-
-
-var resToJSON = (array) => {
-	var toWrite = {};
-	for (var i = 0; i < array.length; i++) {
-		var baseUrl = getBaseUrl(array[i]);
-		toWrite[baseUrl] = true;
-	}
-	fs.writeFile('baseUrls.json', JSON.stringify(toWrite), (err) => {
-		if (err) {
-			console.log(err);
 		} else {
-			console.log('Success!');
+			var startLength = queue.length;
+			var traverseArray = (count) => {
+				if (baseUrls[getBaseUrl(queue[count])]) {
+					console.log(queue[count]);
+				}
+				var next = count + numCPUs >= startLength ? count + 1 : count + numCPUs;
+				if (queue[next]) {
+					traverseArray(next);
+				} else {
+					cluster.worker.kill();
+				}
+			};
+			var id = cluster.worker.id - 1;
+			traverseArray(id);
 		}
-	});
+	},
+	scheduleCrawlersSingle: (queue) => {
+		while (queue.length > 0) {
+			var url = queue.shift();
+			if (baseUrls[getBaseUrl(url)]) {
+				//crawl not yet defined
+				crawl(queue.shift(), (result) => {
+					queue.concat(result);
+				});
+			}
+		}
+	}
 };
 
-var whiteListKeys = Object.keys(whitelist).slice(0,10);
-var startTime = new Date().getTime();
-frontPageCrawler.getNewBlogPostsSingleThread(whiteListKeys, scheduleCrawlers);
-//frontPageCrawler.getNewBlogPosts(whiteListKeys, scheduleCrawlers);
-// frontPageCrawler.getNewBlogPosts(whiteListKeys, (result) => {
-// 	console.log(new Date().getTime() - startTime);
-// 	console.log(result.length);
-// });
+
+
+
+
+
 
