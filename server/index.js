af7dc724a1c57f79890260b70c36414840090081
oldchevy <nick.olszowy@gmail.com>
2016-09-29 16:48:23 -0700

    Set up production port

--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
 var app = require('./server.js');
-var port = 3000;
+var port = process.env.RDS_PORT || 3000;
 
 app.listen(port, function () {
   console.log('BlogRank RESTful API listening on port ' + port);
