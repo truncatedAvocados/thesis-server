d623058314f776c76bb97e224313ae05dc13e6a2
p-herbert <pete.a.herbert@gmail.com>
2016-09-29 16:00:26 -0700

    (test) Create tests for single thread scheduler

---
+++ test/unit/scheduler.singleThread.test.js
@@ -0,0 +1,9 @@
+const assert = require('assert');
+const scheduler = require('../../scheduler');
+const links = require('../data/links').slice(0, 1);
+
+describe('Single Thread Scheduler Tests', () => {
+  describe('Check methods exist', () => {
+  });
+});
+
