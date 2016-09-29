d262cfdfbdbc75a0ce906ae28b090a8497b44305
abandeali1 <bandeali1@gmail.com>
2016-09-29 15:55:32 -0700

    (feat) Add versions of scheduler and crawler that use 2 way communication between master and children

--- scheduler.js
+++ scheduler.js
@@ -8,7 +8,7 @@ var getBaseUrl = (url) => {
 };
 
 module.exports = {
-	scheduleCrawlers: (queue) => {
+	scheduleCrawlersMulti: (queue) => {
 		if (cluster.isMaster) {
 			var workers = numCPUs;
 			for (var i = 0; i < workers; i++) {
@@ -29,6 +29,67 @@ module.exports = {
 			};
 			var id = cluster.worker.id - 1;
 			traverseArray(id);
+		}
+	},
+	scheduleCrawlersMulti2: (urlList) => {
+		if (cluster.isMaster) {
+			//var workers = numCPUs;
+			var workers = 8;
+			var urlCount = -1;
+			var childMessageHandler = (message) => {
+				if (message.type = 'finish') {
+					urlCount++;
+					urlList = urlList.concat(message.data);
+					if (urlList[urlCount]) {
+						cluster.workers[message.from].send({
+							type: 'start',
+							from: 'master',
+							data: urlList[urlCount],
+							count: urlCount
+						});
+					} else {
+						cluster.workers[message.from].send({
+							type: 'kill',
+							from: 'master'
+						});
+					}
+				}
+			};
+			var createChild = () => {
+				var child = cluster.fork();
+				child.on('message', childMessageHandler);
+			};
+			for (var i = 0; i < workers; i++) {
+				createChild();
+			}
+			cluster.on('disconnect', (worker) => {
+				workers--;
+				if (workers === 0) {
+					cluster.disconnect(() => {
+						console.log('Finished');
+					});
+				}
+			});
+		} else {
+			var masterMessageHandler = (message) => {
+				if (message.type === 'start') {
+					console.log(message.data);
+					//crawl(message.data), send to master
+					process.send({
+						type: 'finish',
+						from: cluster.worker.id,
+						data: []
+					});
+				} else if (message.type === 'kill') {
+					cluster.worker.kill();
+				}
+			};
+			process.on('message', masterMessageHandler);
+			process.send({
+				type: 'finish',
+				from: cluster.worker.id,
+				data: []
+			});
 		}
 	},
 	scheduleCrawlersSingle: (queue) => {
