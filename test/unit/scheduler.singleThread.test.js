28022ff236691a52eaf14b09b3d1942e21b14555
p-herbert <pete.a.herbert@gmail.com>
2016-09-29 16:15:30 -0700

    (test) Check scheduleCrawlersSingle exists

--- test/unit/scheduler.singleThread.test.js
+++ test/unit/scheduler.singleThread.test.js
@@ -1,9 +1,24 @@
 const assert = require('assert');
-const scheduler = require('../../scheduler');
-const links = require('../data/links').slice(0, 1);
+const scheduler = require('../../scheduler').scheduleCrawlersSingle;
+const links = require('../data/links').slice(0, 10);
 
 describe('Single Thread Scheduler Tests', () => {
   describe('Check methods exist', () => {
+    it('Check scheduler property scheduleCrawlersSingle exists' , () => {
+      if (scheduler === undefined) {
+        assert.ok(false);
+      } else {
+        assert.ok(true);
+      }
+    });
+
+    it('Check scheduler property scheduleCrawlersSingle is a function', () => {
+      if(typeof scheduler !== 'function') {
+        assert.ok(false);
+      } else {
+        assert.ok(true);
+      }
+    });
   });
 });
 
