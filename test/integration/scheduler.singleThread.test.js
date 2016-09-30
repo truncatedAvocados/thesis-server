dc786f9d9804fa416381b859e01275894d54d84d
p-herbert <pete.a.herbert@gmail.com>
2016-09-29 18:16:13 -0700

    (test) Create instance of PostCrawler

--- test/integration/scheduler.singleThread.test.js
+++ test/integration/scheduler.singleThread.test.js
@@ -4,3 +4,19 @@ const scheduler = require('../../scheduler').scheduleCrawlersSingle;
 const links = require('../data/links.json').slice(0, 10);
 const db = require('../../db/database');
 
+describe('Single Thread Scheduler with PostCrawler Integration Tests', () => {
+  let crawler;
+
+  it('Create an instance of PostCrawler for each link', () => {
+    scheduler(links, (link) => {
+      crawler = new PostCrawler(link.url);
+
+      if (crawler === undefined) {
+        assert.ok(false);
+      }
+    });
+
+    assert.ok(true);
+  });
+});
+
