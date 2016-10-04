7adb036e47fe4a65bdd94cf1f366839e0a3080c5
oldchevy <nick.olszowy@gmail.com>
2016-10-03 17:03:48 -0700

    Integrate serving client code

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ app.use(bodyParser.json());
 // - common: standard apache with less info
 // - dev: color coded information
 app.use(morgan('dev'));
-app.use(express.static('./client'));
+app.use(express.static('thesis-client'));
 
 //Include rate limiting middleware, you can also specify it as a middleware function in any route,
 //as well as customize many of the actions taken
@@ -28,11 +28,18 @@ limiter({
   expire: 1000 * 60 * 60
 });
 
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+
 // Import the outer and assign it to the correct route:
 app.use('/api', routes);
 
-app.get('/', function (req, res) {
-  res.json({ message: 'Welcome to the BlogRank RESTful API!' });
-});
+// app.get('/', function (req, res) {
+//   res.json({ message: 'Welcome to the BlogRank RESTful API!' });
+// });
 
 module.exports = app;
