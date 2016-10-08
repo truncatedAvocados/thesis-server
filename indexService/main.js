c979a507ce96205539d5001fe8150e0332897048
p-herbert <pete.a.herbert@gmail.com>
2016-10-08 14:45:19 -0700

    (feat) Implement Power method

--- indexService/main.js
+++ indexService/main.js
@@ -102,6 +102,31 @@ const norm = (v, p) => {
   return Math.pow(sum, 1 / p);
 };
 
+const solver = (M, d, error) => {
+  const N = M.size()[1];
+  const ones = math.ones(N, N);
+
+  // Vector of ranks for the ith node, scaled between [0, 1]
+  let v = math.zeros(N, 1).map((value, index) => { return Math.random(); });
+
+  // Normalize
+  v = math.multiply(v, 1 / norm(v, 1));
+
+  // Initial solution
+  let lastV = math.ones(N, 1);
+
+  // Transition matrix
+  const Mhat = math.add(math.multiply(d, M), math.multiply((1 - d) / N, ones));
+
+  // Power method with convergence in L-2
+  while (norm(math.subtract(v, lastV), 2) > error) {
+    lastV = v;
+    v = math.multiply(Mhat, v);
+  }
+
+  return v;
+};
+
 // Page Rank
 const rankPages = (cb) => {
   Post.findAndCountAll()
