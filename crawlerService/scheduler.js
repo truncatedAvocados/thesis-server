afce45400fbbfb0f8747bd8325bdfe383f714db2
oldchevy <nick.olszowy@gmail.com>
2016-10-08 17:17:34 -0700

    (feat) Finish basic functionality of interactive mode, now must integrate DB

--- crawlerService/scheduler.js
+++ crawlerService/scheduler.js
@@ -6,101 +6,133 @@ const path = require('path');
 const crawlUrl = require('./postCrawler').crawlUrl;
 
 module.exports = {
-	scheduleCrawlersMulti: (urlList, callback) => {
-		if (cluster.isMaster) {
-			cluster.setupMaster({
+  scheduleCrawlersMulti: (urlList, callback) => {
+    if (cluster.isMaster) {
+      cluster.setupMaster({
         exec: path.join(__dirname, 'childProc.js')
       });
-			var crawled = {};
-			var workers = numCPUs * 2 < urlList.length ? numCPUs * 2 : urlList.length;
-			var urlCount = -1;
-			ON_DEATH((signal, err) => {
-				fs.writeFile('queue.json', JSON.stringify(urlList.slice(urlCount)), (err) => {
-					if (err) {
-						console.log(err);
-					}
-				});
-			});
-			var childMessageHandler = (message) => {
-				if (message.type === 'ready') {
-					urlCount++;
-					cluster.workers[message.from].send({
-						type: 'start',
-						from: 'master',
-						data: urlList[urlCount],
-						count: urlCount
-					});
-				} else if (message.type = 'finish') {
-					crawled[urlList[message.count].url] = true;
-					urlList = urlList.concat(message.data);
-					urlCount++;
-					if (urlList[urlCount]) {
-						if (crawled[urlList[urlCount].url]) {
-							urlList[urlCount].url = null;
-						}
-						cluster.workers[message.from].send({
-							type: 'start',
-							from: 'master',
-							data: urlList[urlCount],
-							count: urlCount
-						});
-					} else {
-						cluster.workers[message.from].send({
-							type: 'kill',
-							from: 'master'
-						});
-					}
-				}
-			};
-			var createChild = () => {
-				var child = cluster.fork();
-				child.on('message', childMessageHandler);
-			};
-			for (var i = 0; i < workers; i++) {
-				createChild();
-			}
-			cluster.on('disconnect', (worker) => {
-				workers--;
-				if (workers <= 0) {
-					cluster.disconnect(() => {
-						console.log('Finished');
-						callback(new Date().getTime());
-					});
-				}
-			});
-		}
-	},
-  scheduleCrawlers: (urlList, callback, crawled) => {
-  	if (urlList.length === 0) {
-  		console.log('Finished');
-  		callback(new Date().getTime());
-  		return;
-  	}
-  	var scheduleCrawlers = module.exports.scheduleCrawlers;
+      var crawled = {};
+      var workers = numCPUs * 2 < urlList.length ? numCPUs * 2 : urlList.length;
+      var urlCount = -1;
+      ON_DEATH((signal, err) => {
+        fs.writeFile('queue.json', JSON.stringify(urlList.slice(urlCount)), (err) => {
+          if (err) {
+            console.log(err);
+          }
+        });
+      });
+      var childMessageHandler = (message) => {
+        if (message.type === 'ready') {
+          urlCount++;
+          cluster.workers[message.from].send({
+            type: 'start',
+            from: 'master',
+            data: urlList[urlCount],
+            count: urlCount
+          });
+        } else if (message.type = 'finish') {
+          crawled[urlList[message.count].url] = true;
+          urlList = urlList.concat(message.data);
+          urlCount++;
+          if (urlList[urlCount]) {
+            if (crawled[urlList[urlCount].url]) {
+              urlList[urlCount].url = null;
+            }
+            cluster.workers[message.from].send({
+              type: 'start',
+              from: 'master',
+              data: urlList[urlCount],
+              count: urlCount
+            });
+          } else {
+            cluster.workers[message.from].send({
+              type: 'kill',
+              from: 'master'
+            });
+          }
+        }
+      };
+      var createChild = () => {
+        var child = cluster.fork();
+        child.on('message', childMessageHandler);
+      };
+      for (var i = 0; i < workers; i++) {
+        createChild();
+      }
+      cluster.on('disconnect', (worker) => {
+        workers--;
+        if (workers <= 0) {
+          cluster.disconnect(() => {
+            console.log('Finished');
+            callback(new Date().getTime());
+          });
+        }
+      });
+    }
+  },
+  scheduleCrawlers: (urlList, options, callback, crawled) => {
+    if (urlList.length === 0) {
+      console.log('Finished');
+      callback(new Date().getTime());
+      return;
+    }
+    console.log(urlList[0]);
+    var scheduleCrawlers = module.exports.scheduleCrawlers;
     var count = 0;
     var result = [];
     var crawled = crawled || {};
 
     ON_DEATH((signal, err) => {
-    	fs.writeFile('queue.json', JSON.stringify(urlList.slice(count).concat(result)), (err) => {
-    		if (err) {
-    			console.log(err);
-    		}
-    	});
+      fs.writeFile('queue.json', JSON.stringify(urlList.slice(count).concat(result)), (err) => {
+        if (err) {
+          console.log(err);
+        }
+      });
     });
 
     urlList.forEach((url) => {
-    	crawlUrl(url, (links, index) => {
-    		if (!crawled[url]) {
-	    		crawled[url] = true;
-	    		result = result.concat(links);
-	    	}
-	    	count++;
-    		if (count === urlList.length) {
-    			scheduleCrawlers(result, callback, crawled);
-    		}
-    	});
+      crawlUrl(url, options, (links, index) => {
+        console.log('Concatting links: ', links);
+        if (!crawled[url]) {
+          crawled[url] = true;
+          result = result.concat(links);
+        }
+
+        count++;
+        console.log('Callback status: ', count, urlList.length);
+        if (count === urlList.length) {
+          scheduleCrawlers(result, options, callback, crawled);
+        }
+      });
     });
-  } 
+  },
+
+  scheduleCrawlersInteractive: (urlList, options, callback, crawled, tracker) => {
+    tracker = tracker || 0;
+    console.log(tracker, urlList.length);
+    if (tracker === urlList.length) {
+      console.log('Finished');
+      callback(new Date().getTime());
+      return;
+    }
+
+    console.log(urlList[tracker]);
+    var scheduleCrawlersInteractive = module.exports.scheduleCrawlersInteractive;
+    var crawled = crawled || {};
+
+
+
+    crawlUrl(urlList[tracker], options, (links, index) => {
+      var result = urlList.slice();
+      console.log(crawled, !crawled[urlList[tracker].url]);
+      if (!crawled[urlList[tracker].url]) {
+        crawled[urlList[tracker].url] = true;
+        var result = result.concat(links);
+        console.log('Concatting links: ', result, result.length);
+      }
+
+      scheduleCrawlersInteractive(result, options, callback, crawled, tracker + 1);
+    });
+  }  
 };
 
--- crawlerService/startCrawlers.js
+++ crawlerService/startCrawlers.js
@@ -3,9 +3,13 @@ const crawlUrl = require('./postCrawler').crawlUrl;
 const scheduler = require('./scheduler');
 const whitelist = require('./whitelist.json');
 const prompt = require('prompt');
+const colors = require('colors/safe');
+prompt.message = colors.underline.green('BlogRank');
+prompt.delimiter = colors.green(' <-=-=-=-=-=-> ');
+
 
 const whiteListKeys = Object.keys(whitelist);
-const startTime = new Date().getTime();
+const startTime = new Date();
 
 const start = () => {
   if (process.argv.indexOf('--continue') > -1) {
@@ -18,32 +22,61 @@ const start = () => {
 
     var properties = [
       {
-        name: 'WEBSITE STRING', 
+        message: 'ENTER INTERACTIVE MODE? (Y/n)',
+        name: 'decision', 
         validator: /^[y|n]+$/,
-        warning: 'Just say yes or no man (y,n)'
+        warning: colors.red('Just say yes or no man (y,n)')
       }
     ];
 
     prompt.start();
 
     prompt.get(properties, function (err, result) {
-      if (err) { return onErr(err); }
-      console.log('Command-line input received:');
-      console.log('  Username: ' + result.username);
-      console.log('  Password: ' + result.password);
-    });
+      if (err) { 
+        console.log(err);
+        return 1;
+      }
+      if (result.decision === 'y') {
+        console.log(colors.rainbow('\nYoU jUsT eNtErEd InTeRaCtIvE mOdE!!@@#$!!!'));
+        console.log('Getting some random front page posts');
 
-    function onErr(err) {
-      console.log(err);
-      return 1;
-    }
+        randomKeys = [];
+        for (var i = 0; i < 1; i++) {
+          var randomSite = whiteListKeys[Math.floor(Math.random() * whiteListKeys.length)];
+          if (randomKeys.indexOf(randomSite) < 0) {
+            randomKeys.push(randomSite);
+          }
+        }
+
+        console.log(randomKeys);
+
+        frontPageCrawler.getPosts(randomKeys, (results) => {
+
+          console.log('POSTS FROM FRONT PAGE:' + results.length);
+          results = results.map(result => {
+            return {
+              url: result,
+              parent: null
+            };
+          });
+          scheduler.scheduleCrawlersInteractive(results, {interactive: true}, (time) => {
+            console.log('You just did this for ' + (new Date() - startTime / (60 * 1000)) + ' minutes');
+          });
+
+        });
+
+      } else {
+        console.log(colors.magenta('Nevermind >:('));
+        return 1;
+      }
+    });
 
   } else {
     frontPageCrawler.getPosts(whiteListKeys, (results) => {
       console.log('FRONT PAGE POSTS: ', results.length);
       frontPageCrawler.filterPosts(results, (filtered) => {
         console.log('FILTERED POSTS: ', filtered.length);
-        // scheduler.scheduleCrawlers(filtered, (time) => {
+        // scheduler.scheduleCrawlers(filtered, null, (time) => {
         //   console.log(time - startTime);
         // });
         scheduler.scheduleCrawlersMulti(filtered, (time) => {
